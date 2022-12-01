6c6fc006f1c626a81e8ec996c7a13aa805cb672f
Stefan Markovsky <stefan.markovsky@student.kuleuven.be>
2022-12-01 11:52:57 +0100

    better version

--- day1/calories2.py
+++ day1/calories2.py
@@ -1,20 +1,3 @@
-def partition(array, low, high):
- 
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-        if array[j] >= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
-
-def quickSort(array, low, high):
-    if low < high:
-        pi = partition(array, low, high)
-        quickSort(array, low, pi - 1)
-        quickSort(array, pi + 1, high)
-
 def sort(array):
     i = len(array) - 1
     while i > 0 and array[i] > array[i-1]:
@@ -35,7 +18,6 @@ def main():
         else:
             cal += int(line)
     f.close()
-    #quickSort(calories, 0, len(calories) - 1)
     result = calories[0]
     print(result)
     for i in range(1, 3):
