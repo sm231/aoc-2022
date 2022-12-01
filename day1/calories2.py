327bd89f3d2dcbb0c2eeb283a97008d468ff5e67
Stefan Markovsky <stefan.markovsky@student.kuleuven.be>
2022-12-01 12:04:03 +0100

    comments

--- day1/calories2.py
+++ day1/calories2.py
@@ -12,12 +12,15 @@ def main():
     cal = 0
     for line in f:
         if line == '\n':
+            # calories of the next elf. So append to the list and sort list with insertionSort on the last index. Carry on by resetting cal to 0
             calories.append(cal)
             sort(calories)
             cal = 0
         else:
+            # sum the total calories of the elf
             cal += int(line)
     f.close()
+    #calories is a sorted array from biggest to smallest for the elfs.
     result = calories[0]
     print(result)
     for i in range(1, 3):
